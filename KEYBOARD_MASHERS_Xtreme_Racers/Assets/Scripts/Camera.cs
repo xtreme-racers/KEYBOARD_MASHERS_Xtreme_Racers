f640a1f99521fca5daf761ed52d8d3b603facddc
Aly Husein <aly.husein25@gmail.com>
2020-11-03 01:24:19 +0200

    Add files via upload

--- KEYBOARD_MASHERS_Xtreme_Racers/Assets/Scripts/Camera.cs
+++ KEYBOARD_MASHERS_Xtreme_Racers/Assets/Scripts/Camera.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    public Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(target.position.x, target.position.y, -10f);
+
     }
 }
+
--- KEYBOARD_MASHERS_Xtreme_Racers/Assets/Scripts/CarController.cs
+++ KEYBOARD_MASHERS_Xtreme_Racers/Assets/Scripts/CarController.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections;
+﻿// CODE FOUND ONLINE
+/*
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
 
-    public float power = 3;
-    public float maxspeed = 5;
-    public float turnpower = 2;
+    public float power = 15;
+    public float maxspeed = 10;
+    public float turnpower = 3;
     public float friction = 3;
     public Vector2 curspeed;
-    Rigidbody2D rigidbody2D;
+    new Rigidbody2D rigidbody2D;
 
     // Use this for initialization
     void Start()
@@ -48,25 +50,179 @@ public class CarController : MonoBehaviour
             transform.Rotate(Vector3.forward * -turnpower);
         }
 
-        noGas();
+    }
+
 
     }
 
-    void noGas()
+// HAORANS CODE
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float maxSpeed;
+    public float acceleration;
+    public float deacceleration;
+    public float steering;
+
+    private Rigidbody2D rb;
+    private float currentSpeed;
+
+    void Start()
     {
-        bool gas;
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        rb = GetComponent<Rigidbody2D>();
+        //Debug.Log('start');
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = -Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 speed;
+
+        if (v > 0)
         {
-            gas = true;
+            // pressing up key
+            speed = transform.up * (v * acceleration);
+        }
+        else if (v < 0)
+        {
+            // pressing down key
+            speed = transform.up * (v * deacceleration);
         }
         else
         {
-            gas = false;
+            // release all keys
+            speed = Vector2.zero;
         }
 
-        if (!gas)
+
+        // if(speed != Vector2.zero){
+        //     rb.AddForce(speed);
+        // }else{
+        //     Debug.Log("here");
+        //     rb.AddForce(-speed);
+
+        // }
+
+        rb.AddForce(speed);
+
+        float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
+        if (direction >= 0.0f)
         {
-            rigidbody2D.drag = friction * 2;
+            rb.rotation += h * steering * (rb.velocity.magnitude / 5.0f);
         }
+        else
+        {
+            rb.rotation -= h * steering * (rb.velocity.magnitude / 5.0f);
+        }
+
+        Vector2 forward = new Vector2(0.0f, 0.5f);
+        float steeringRightAngle;
+        if (rb.angularVelocity > 0)
+        {
+            steeringRightAngle = -90;
+        }
+        else
+        {
+            steeringRightAngle = 90;
+        }
+
+        Vector2 rightAngleFromForward = Quaternion.AngleAxis(steeringRightAngle, Vector3.forward) * forward;
+        Debug.DrawLine((Vector3)rb.position, (Vector3)rb.GetRelativePoint(rightAngleFromForward), Color.green);
+
+        float driftForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(rightAngleFromForward.normalized));
+
+        Vector2 relativeForce = (rightAngleFromForward.normalized * -1.0f) * (driftForce * 10.0f);
+
+        rb.AddForce(rb.GetRelativeVector(relativeForce));
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+        currentSpeed = rb.velocity.magnitude;
+
+        //Debug.Log(speed);
     }
+}
+*/
+
+// RigidBody 2D settings:
+// Body Type: Dynamic
+// Mass: 1
+// Linear Drag: 2
+// Angular Drag: 1
+// Gravity Scale: 0
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    //public float maxSpeed;
+    public float acceleration = 25f;
+    public float steeringPower = 200f;
+    public float driftFactor_Stick = 0.9f;
+    //public float driftFactor_Slip;
+    //public float maxStickVelocity;
+    //public float minSlipVelocity;
+
+
+    float steering, speed, direction;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //Debug.Log('start');
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        float h = -Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Debug.Log(rightVelocity().magnitude);
+
+        float driftFactor = driftFactor_Stick;
+
+
+        rb.velocity = forwardVelocity() + rightVelocity() * driftFactor;
+
+        rb.AddForce(transform.up * v * acceleration);
+        // go forward
+
+        float t = Mathf.Lerp(0, steeringPower, rb.velocity.magnitude / 5);
+        // if speed is below 5 than steering power will drop to 0
+
+        rb.angularVelocity = h * t;
+        // turning
+    }
+
+    Vector2 forwardVelocity()
+    {
+        // Return only the forward direction. 
+        // Reset sideway velocity
+        // dot product of two vectors
+        return transform.up * Vector2.Dot(rb.velocity, transform.up);
+    }
+
+    Vector2 rightVelocity()
+    {
+        // dot product of two vectors
+        return transform.right * Vector2.Dot(rb.velocity, transform.right);
+    }
+
+
 }
