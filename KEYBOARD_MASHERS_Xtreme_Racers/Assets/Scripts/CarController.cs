1b547ace73d92ea9e105154e4d5889441695b7cd
Haoran He <hrgutou@gmail.com>
2020-11-02 01:15:14 -0500

    Finish car controller script and camera follow script

--- KEYBOARD_MASHERS_Xtreme_Racers/Assets/Scripts/CarController.cs
+++ KEYBOARD_MASHERS_Xtreme_Racers/Assets/Scripts/CarController.cs
@@ -6,6 +6,7 @@
 // Gravity Scale: 0
 
 
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,7 +25,6 @@ public class CarController : MonoBehaviour
     float steering, speed, direction;
     
     private Rigidbody2D rb;
-    //private float currentSpeed;
     
     void Start () {
         rb = GetComponent<Rigidbody2D>();
@@ -32,66 +32,6 @@ public class CarController : MonoBehaviour
 
     }
  
-    // void FixedUpdate () {
-    //     float h = -Input.GetAxis("Horizontal");
-    //     float v = Input.GetAxis("Vertical");
-
-    //     Vector2 speed;
-        
-    //     if(v>0){
-    //         // pressing up key
-    //         speed = transform.up * (v * acceleration);
-    //     }else if(v<0){
-    //         // pressing down key
-    //         speed = transform.up * (v * deacceleration);
-    //     }else{
-    //         // release all keys
-    //         speed = Vector2.zero;
-    //     }
-        
-
-    //     // if(speed != Vector2.zero){
-    //     //     rb.AddForce(speed);
-    //     // }else{
-    //     //     Debug.Log("here");
-    //     //     rb.AddForce(-speed);
-
-    //     // }
-
-    //     rb.AddForce(speed);
-
-    //     float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
-    //     if(direction >= 0.0f) {
-    //         rb.rotation += h * steering * (rb.velocity.magnitude / 5.0f);
-    //     } else {
-    //         rb.rotation -= h * steering * (rb.velocity.magnitude / 5.0f);
-    //     }
-
-    //     Vector2 forward = new Vector2(0.0f, 0.5f);
-    //     float steeringRightAngle;
-    //     if(rb.angularVelocity > 0) {
-    //         steeringRightAngle = -90;
-    //     } else {
-    //         steeringRightAngle = 90;
-    //     }
-
-    //     Vector2 rightAngleFromForward = Quaternion.AngleAxis(steeringRightAngle, Vector3.forward) * forward;
-    //     Debug.DrawLine((Vector3)rb.position, (Vector3)rb.GetRelativePoint(rightAngleFromForward), Color.green);
-
-    //     float driftForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(rightAngleFromForward.normalized));
-
-    //     Vector2 relativeForce = (rightAngleFromForward.normalized * -1.0f) * (driftForce * 10.0f);
-
-    //     rb.AddForce(rb.GetRelativeVector(relativeForce));
-
-    //     if (rb.velocity.magnitude > maxSpeed){
-    //         rb.velocity = rb.velocity.normalized * maxSpeed;
-    //     }
-    //     currentSpeed = rb.velocity.magnitude;
-
-    //     //Debug.Log(speed);
-    // }
-    
 
     private void FixedUpdate() {
         float h = -Input.GetAxis("Horizontal");
